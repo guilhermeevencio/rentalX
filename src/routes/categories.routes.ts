699ac74390b84a20e0871a4b6b1e1e75e193d6a7
Guilherme <guilhermeevencioluz@gmail.com>
2022-05-15 15:50:07 -0300

    feat: add the '/categories' route

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+
+const categoriesRoutes = Router();
+
+const categories = [];
+
+categoriesRoutes.post('/', (request, response) => {
+  const { name, description } = request.body;
+
+  const category = {
+    name,
+    description,
+  };
+  categories.push(category);
+  return response.status(201).send();
+});
+
+export { categoriesRoutes };
